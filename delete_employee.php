4aa54cbf0e2ac787e11e72aac925382fe3805e83
Gibson100 <gibsonmumba100@gmail.com>
2022-03-19 15:22:14 +0530

    added deletion of image from assets\images folder when user is deleted

--- delete_employee.php
+++ delete_employee.php
@@ -5,6 +5,22 @@ if (isset($_REQUEST['delete_id']) && $_REQUEST['page'] === 'list-users')
 
     $id = $_REQUEST['delete_id'];
 
+    // get the abs path to the assets folder 
+    $path = dirname(__FILE__);
+    $abspath = $path . '\assets\\images\\';
+
+    // get the image name from the database
+
+    $data = $wpdb->get_results("SELECT Image FROM `wp_gkblabs_employees` WHERE `id` = '$id'");
+
+    foreach($data as $result)
+    {
+        if ($result->Image !== 'no image.png')
+        {
+            unlink($abspath . "$result->Image");
+        }
+    }
+
     $wpdb->query("DELETE FROM `wp_gkblabs_employees` WHERE `id` = '$id'");
 
     echo "<script>alert('record deleted successfully')</script>";
